7f486cc3456c3d750aeb4c8f6ab5e3a717d3fafa
wojo1286 <wojo1286@gmail.com>
2025-10-15 17:00:48 -0600

    Fix setup.sh to create config.toml correctly

--- setup.sh
+++ setup.sh
@@ -1,14 +1,10 @@
 mkdir -p ~/.streamlit/
-echo "
-
-[server]\n
-
-headless = true\n
-
-port = $PORT\n
-
-enableCORS = false\n
-
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
 " > ~/.streamlit/config.toml
 
+# This is the command that installs the browser
 playwright install chromium
